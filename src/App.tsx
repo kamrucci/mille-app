9dfa8165aa11a648f6c59ddc965354ddc2a3e34d
kamrowski <lukasz@kamrowsky.pl>
2023-08-05 15:14:52 +0200

    imported utils

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { add } from '@kamrucci/utils';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <h1>{add(1, 3)}</h1>
       </header>
     </div>
   );
